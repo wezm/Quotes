af512f6b89f9f454f98599ae6b72e9e7f1c000da
Wesley Moore <wez@wezm.net>
2009-11-02 20:20:05 +1100

    Add dates to quotes and restore userlist

--- views/index.rb
+++ views/index.rb
@@ -13,9 +13,11 @@ module Quotes
 
         def quotes
           @quotes.map do |q|
+            created = q.created_at ? { :date => q.created_at.strftime('%a %d %b %Y %I:%M %p') } : false
             {
               :body => q.quote_body,
               :quotee => q.user.username,
+              :created_at => created,
             }
           end
         end
